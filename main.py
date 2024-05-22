4640e66ef8fe6e3817e550298a569bae64fd8635
Elouen <138658525+Yoshkacafe@users.noreply.github.com>
2024-05-22 14:01:40 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyxel
+
+class App:
+    def __init__(self) -> None:
+        pyxel.init(128, 128, title="Nuit Du Code")
+        self.x = 0
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
+
+App()
